b5a898aca25046b883ad796e00404ed940f423e7
Chur <danielseguro23@gmail.com>
2022-03-04 11:28:03 -0500

    Modificacion observable.h

--- observer/observer.h
+++ observer/observer.h
@@ -1,13 +1,14 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
-typedef struct __observer {
-	void (*destroy)(struct __observer *);
-	void* impl;
-	void (*notify)(struct __observer*, void *);
-	void (*notifyImpl)(void*, int, void*);
+typedef struct __observer
+{
+    void (*destroy)(struct __observer *);
+    void *impl;
+    void (*notify)(struct __observer *, int, void *);
+    void (*notifyImpl)(void *, int, void *);
 } Observer;
 
-Observer * observer_new(void*, void (*)(void*, int, void*));
+Observer *observer_new(void *, void (*)(void *, int, void *));
 
-#endif 
+#endif
