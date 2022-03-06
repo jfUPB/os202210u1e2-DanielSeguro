e246b3b5efc55b66b38acf1cd940d9d0a9227ba1
Chur <danielseguro23@gmail.com>
2022-03-06 11:21:53 -0500

    Implementacion impresora

---
+++ ejemplo/impresora.c
@@ -0,0 +1,39 @@
+#include "impresora.h"
+static void _error(Impresora *this, char *error)
+{
+    this->event = IMPRESORA_ERROR;
+    this->error = error;
+    this->observable->notifyObservers(this->observable);
+}
+
+static int _registerObserver(Impresora *this, Observer *observer)
+{
+    return this->observable->registerObserver(this->observable, observer);
+}
+
+static int _unregisterObserver(Impresora *this, Observer *observer)
+{
+    return this->observable->unregisterObserver(this->observable, observer);
+}
+
+static void _destroy(Impresora *this)
+{
+    this->observable->destroy(this->observable);
+
+    if (NULL != this)
+    {
+        free(this);
+        this = NULL;
+    }
+}
+
+Impresora *IMP_create(char *name)
+{
+    Impresora *this = (Impresora *)malloc(sizeof(*this));
+
+    this->observable = observable_new(this, 1);
+    this->registerObserver = _registerObserver;
+    this->unregisterObserver = _unregisterObserver;
+
+    return this;
+}
--- ejemplo/impresora.h
+++ ejemplo/impresora.h
@@ -1,18 +1,28 @@
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include "../codigo/observer.h"
-  #include "../codigo/observable.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../codigo/observer.h"
+#include "../codigo/observable.h"
 
-typedef enum __impresora_event {
-	IMPRESORA_ERROR,
+#ifndef IMPRESORA_H
+#define IMPRESORA_H
+typedef enum __impresora_event
+{
+    IMPRESORA_ERROR,
 } ImpresoraEvent;
 
-	char* error;
-	ImpresoraEvent event;
-	ImpresoraEvent (*getEvent)(struct __impresora*);
-	char* (*getError)(struct __impresora*);
+typedef struct __impresora
+{
+   
+char *error;
+ImpresoraEvent event;
+ImpresoraEvent (*getEvent)(struct __impresora *);
+char *(*getError)(struct __impresora *);
 
-	//Metodos
-	Observable * observable;
-	int (*registerObserver)(struct __impresora*, Observer*);
-	int (*unregisterObserver)(struct __impresora *, Observer*);
+// Metodos
+Observable *observable;
+int (*registerObserver)(struct __impresora *, Observer *);
+int (*unregisterObserver)(struct __impresora *, Observer *);
+} Impresora;
+
+Impresora* Imp_create(char *)
+#endif
